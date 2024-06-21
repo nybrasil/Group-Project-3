e332c248057ffad083a850a9d0fc8cdd0dc7ff58
Ross Tauchert <57552771+rdtauche@users.noreply.github.com>
2024-06-21 08:02:39 -0400

    fixed trading widget container side with css

--- src/components/TradingViewWidget.jsx
+++ src/components/TradingViewWidget.jsx
@@ -1,45 +1,35 @@
-import "../styles/TradingViewWidget.css";
+import React, { useEffect, useRef } from 'react';
 
-// TradingViewWidget.jsx
-import React, { useEffect, useRef, memo } from 'react';
+function CustomTradingViewWidget() {
+  const container = useRef(null);
 
-function TradingViewWidget() {
-  const container = useRef();
-
-  useEffect(
-    () => {
+  useEffect(() => {
+    // Ensure the script is only appended if the container is not null
+    if (container.current) {
       const script = document.createElement("script");
       script.src = "https://s3.tradingview.com/external-embedding/embed-widget-advanced-chart.js";
-      script.type = "text/javascript";
       script.async = true;
-      script.innerHTML = `
-        {
-          "autosize": true,
-          "symbol": "NASDAQ:AAPL",
-          "interval": "D",
-          "timezone": "Etc/UTC",
-          "theme": "light",
-          "style": "1",
-          "locale": "en",
-          "allow_symbol_change": true,
-          "calendar": false,
-          "support_host": "https://www.tradingview.com"
-        }`;
+      script.innerHTML = JSON.stringify({
+        "autosize": true,
+        "symbol": "NASDAQ:AAPL",
+        "interval": "D",
+        "timezone": "Etc/UTC",
+        "theme": "light",
+        "style": "1",
+        "locale": "en",
+        "allow_symbol_change": true,
+        "calendar": false,
+        "support_host": "https://www.tradingview.com"
+      });
       container.current.appendChild(script);
-    },
-    []
-  );
+    }
+  }, []);
 
   return (
-    <div id="widgetHeight" className="tradingview-widget-container" ref={container} style={{ height: "200%", width: "100%" }}>
-      <div className="tradingview-widget-container__widget" style={{ height: "calc(200% - 32px)", width: "100%" }}></div>
-      <div className="tradingview-widget-copyright"><a href="https://www.tradingview.com/" rel="noopener nofollow" target="_blank"><span className="blue-text">Track all markets on TradingView</span></a></div>
+    <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh' }}>
+      <div ref={container} style={{ width: '80vw', height: '80vh' }}></div>
     </div>
-  );
+      );
 }
 
-export default memo(TradingViewWidget);
-
-
-    
-
+export default React.memo(CustomTradingViewWidget);
