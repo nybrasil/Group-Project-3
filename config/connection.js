2ee745d4846c595b884f1ec99f541ba2f8e8b179
CJ <csh.joo77@gmail.com>
2024-06-27 19:21:59 -0400

    changed config/connection.js so it only calls mongoose.connect(); once, simplified login routes and registration routes in server.js

--- config/connection.js
+++ config/connection.js
@@ -1,5 +1,14 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/techmatchup');
+const mongoURI = 'mongodb://localhost:27017/loginDB';
 
-module.exports = mongoose.connection;
+mongoose.connect(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true });
+
+const db = mongoose.connection;
+
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', () => {
+  console.log('MongoDB connected...');
+});
+
+module.exports = db;
--- server.js
+++ server.js
@@ -2,9 +2,11 @@ const express = require('express');
 const { ApolloServer } = require('@apollo/server');
 const { expressMiddleware } = require('@apollo/server/express4');
 const path = require('path');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
 
 const { typeDefs, resolvers } = require('./schemas');
-const db = require('./config/connection');
+const db = require('./config/connection'); 
 
 const PORT = process.env.PORT || 3001;
 const app = express();
@@ -13,6 +15,21 @@ const server = new ApolloServer({
   resolvers,
 });
 
+
+const userSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  password: {
+    type: String,
+    required: true
+  }
+});
+
+const User = mongoose.model('User', userSchema);
+
 const startApolloServer = async () => {
   await server.start();
   
@@ -20,6 +37,39 @@ const startApolloServer = async () => {
   app.use(express.json());
   
   app.use('/graphql', expressMiddleware(server));
+
+  
+  app.post('/register', async (req, res) => {
+    const { username, password } = req.body;
+
+    const newUser = new User({
+      username,
+      password
+    });
+
+    newUser.save()
+      .then(user => res.send('User registered successfully'))
+      .catch(err => res.status(400).send('Error: ' + err));
+  });
+
+  
+  app.post('/login', async (req, res) => {
+    const { username, password } = req.body;
+
+    User.findOne({ username })
+      .then(user => {
+        if (!user) {
+          return res.status(400).send('User not found');
+        }
+
+        if (user.password === password) {
+          res.send('Login successful');
+        } else {
+          res.status(400).send('Invalid credentials');
+        }
+      })
+      .catch(err => res.status(400).send('Error: ' + err));
+  });
 
   // if we're in production, serve client/dist as static assets
   if (process.env.NODE_ENV === 'production') {
